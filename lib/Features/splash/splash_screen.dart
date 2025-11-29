c03953eb453c836f33695b9bcb5a4def5ef803cd
mohaKrdi <mohmodk20@gmail.com>
2025-11-29 16:27:12 +0300

    added splash screen with animation

---
+++ lib/Features/splash/splash_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/images.dart';
+import '../../core/theme/app_colors.dart';
+import '../../core/theme/text_styles.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+
+  late final AnimationController _controller;
+  late final Animation<double> _opacityAnimation;
+  late final Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.7, curve: Curves.easeIn),
+      ),
+    );
+
+    _scaleAnimation = Tween(begin: 0.8, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.8, curve: Curves.bounceOut),
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [AppColors.primary700, AppColors.gray50],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Opacity(
+                  opacity: _opacityAnimation.value,
+                  child: Column(
+                    children: [
+                      ScaleTransition(
+                        scale: _scaleAnimation,
+                        child: SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: Image.asset(AppImages.kLogoPath),
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      Text(
+                        'Mabeet',
+                        style: AppTextStyles.display1Regular.copyWith(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+
+            const SizedBox(height: 60),
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mabeet/Features/auth/screens/create_account_screen.dart';
+import 'package:mabeet/Features/splash/splash_screen_handler.dart';
 import 'package:mabeet/Features/user/home/screens/tabs.dart';
 import 'package:mabeet/core/theme/app_theme.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-      home: CreateAccountScreen(),
+      home: SplashHandler(),
       themeMode: ThemeMode.system,
     );
   }
