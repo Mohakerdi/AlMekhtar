56122eba902a16da5eadc2e0b8ec90e806ea0bc6
Mohamad Zealnoun@DESKTOP-R631RHL <mzyalnoun@gmail.com>
2025-12-02 12:13:27 +0300

    replace the old property

---
+++ lib/Features/user/bookings/screens/bookings_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mabeet/data/repos/dummy_properties.dart';
+
+class BookingsScreen extends StatelessWidget {
+  const BookingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /* body: ListView.builder(
+        itemCount: dummyProperties.length,
+        itemBuilder: (context, index) {
+          final Property = dummyProperties[index];
+          return 
+        },
+      ),*/
+    );
+  }
+}
