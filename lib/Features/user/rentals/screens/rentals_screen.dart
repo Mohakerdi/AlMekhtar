5017a681ee2e099907d06f09c88eb2dac46c3ca3
Mohamad Zealnoun@DESKTOP-R631RHL <mzyalnoun@gmail.com>
2025-12-02 12:53:06 +0300

    add button add new property

--- lib/Features/user/rentals/screens/rentals_screen.dart
+++ lib/Features/user/rentals/screens/rentals_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:mabeet/Features/user/rentals/widgets/rentals_property.dart';
+import 'package:mabeet/data/repos/dummy_properties.dart';
 
 class RentalsScreen extends StatelessWidget {
   const RentalsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
-        child: Center(child: Text('This is Rentals Page')),
+    return Scaffold(
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: dummyProperties.length,
+        itemBuilder: (context, index) {
+          final property = dummyProperties[index];
+          return RentalsProperty(property: property);
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        //label: Text("add new property"),
+        child: Icon(Icons.add),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
