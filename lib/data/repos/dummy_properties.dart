ff97c3ee2d3d403e3c6e99e1039144b66e015300
Mohamad Zealnoun@DESKTOP-R631RHL <mzyalnoun@gmail.com>
2025-12-02 11:57:20 +0300

    Create a widget folder inside the Booking folder and creat the Property widget

--- lib/data/repos/dummy_properties.dart
+++ lib/data/repos/dummy_properties.dart
@@ -1,10 +1,45 @@
-
 import 'package:mabeet/core/constants/images.dart';
 import 'package:mabeet/data/models/property.dart';
 
 final List<Property> dummyProperties = [
-  Property(id: '1', title: 'Eagle Nest', imageURL: AppImages.kEagleNest, costPerNight: 22.5, location: Location.Damascus,describedLocation: 'Berlin, 1944',description: '',avgRate: 3.5),
-  Property(id: '2', title: 'Twin Tower', imageURL: AppImages.kTwinTowers, costPerNight: 15, location: Location.ALSwieda,describedLocation: 'well, it WAS not is (;',description: '',avgRate: 5.1),
-  Property(id: '3', title: 'CJ\'s House', imageURL: AppImages.kGrooveStreet, costPerNight: 100, location: Location.Tartous,describedLocation: 'Groove Street',description: 'Ba3d el jeser',avgRate: 4.4),
-  Property(id: '4', title: 'Foster\'s House for imaginary friends' , imageURL: AppImages.kFosterHouse, costPerNight: 35, location: Location.RefDemasheq,describedLocation: 'in our hearts',description: 'Cartoon network',avgRate: 3.9)
+  Property(
+    id: '1',
+    title: 'Eagle Nest',
+    imageURL: AppImages.kEagleNest,
+    costPerNight: 22.5,
+    location: Location.Damascus,
+    describedLocation: 'Berlin, 1944',
+    description: '',
+    avgRate: 3.5,
+  ),
+  Property(
+    id: '2',
+    title: 'Twin Tower',
+    imageURL: AppImages.kTwinTowers,
+    costPerNight: 15,
+    location: Location.ALSwieda,
+    describedLocation: 'well, it WAS not is (;',
+    description: '',
+    avgRate: 5.1,
+  ),
+  Property(
+    id: '3',
+    title: 'CJ\'s House',
+    imageURL: AppImages.kGrooveStreet,
+    costPerNight: 100,
+    location: Location.Tartous,
+    describedLocation: 'Groove Street',
+    description: 'Ba3d el jeser',
+    avgRate: 4.4,
+  ),
+  Property(
+    id: '4',
+    title: 'Foster\'s House for imaginary friends',
+    imageURL: AppImages.kFosterHouse,
+    costPerNight: 35,
+    location: Location.RefDemasheq,
+    describedLocation: 'in our hearts',
+    description: 'Cartoon network',
+    avgRate: 3.9,
+  ),
 ];
