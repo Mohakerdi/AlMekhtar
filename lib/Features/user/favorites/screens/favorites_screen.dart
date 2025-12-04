c1dd5c72caab3ca222b53a0d7e58b8a6effc0215
Mohamad Zealnoun@DESKTOP-R631RHL <mzyalnoun@gmail.com>
2025-12-04 14:04:14 +0300

    build favorites screen

--- lib/Features/user/favorites/screens/favorites_screen.dart
+++ lib/Features/user/favorites/screens/favorites_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mabeet/Features/user/favorites/widgets/favorites_property.dart';
+import 'package:mabeet/data/repos/dummy_properties.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
-        child: Center(child: Text('This is Favorites Page')),
+    return Scaffold(
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: dummyProperties.length,
+        itemBuilder: (context, index) {
+          final property = dummyProperties[index];
+          return FavoritesProperty(property: property);
+        },
       ),
     );
   }
--- lib/Features/user/rentals/screens/add_property_screen.dart
+++ lib/Features/user/rentals/screens/add_property_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:mabeet/Features/user/chat/screens/chat_screen.dart';
+import 'package:mabeet/Features/user/notifications/screens/notifications_screen.dart';
 
 class AddPropertyScreen extends StatelessWidget {
   const AddPropertyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("add new property"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (builder) => const NotificationsScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.notifications_none),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (builder) => const Chatscreen()),
+              );
+            },
+            icon: const Icon(Icons.chat),
+          ),
+        ],
+      ),
+    );
   }
 }
