4f495b6fec614a13ef9edf5213548d7cccd665e1
Mohamad Zealnoun@DESKTOP-R631RHL <mzyalnoun@gmail.com>
2025-12-22 12:38:12 +0300

    add cubit to rentals

---
+++ lib/Features/user/rentals/services/rentals_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mabeet/Features/user/rentals/services/rentals_state.dart';
+import 'package:mabeet/data/models/property.dart';
+import 'package:mabeet/data/repos/dummy_properties.dart';
+
+class RentalsCubit extends Cubit<RentalsState> {
+  RentalsCubit() : super(RentalsInitial());
+
+  void loadRentals() async {
+    emit(RentalsLoading());
+
+    try {
+      await Future.delayed(Duration(seconds: 2));
+
+      //throw Exception('Failed to load bookings');
+
+      List<Property> rentals = dummyProperties;
+      emit(RentalsLoaded(rentals));
+    } catch (e) {
+      emit(RentalsError('Failed to load rentals'));
+    }
+  }
+}
